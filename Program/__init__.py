2881457b9278549e3e69fe0cba8ca53dd1893b7e
Niespodzianek <michal_grzyb@yahoo.pl>
2022-08-22 14:53:17 +0200

    Drugi testowy

--- Program/__init__.py
+++ Program/__init__.py
@@ -1,4 +1,7 @@
+from modul_polaczenia.modul_polaczenia import program_1
+
 def program():
+    program_1()
     print('KONIEC PRACY PROGRAMU !!!')
 
 if __name__ == '__main__':
---
+++ Program/modul_polaczenia/modul_polaczenia.py
@@ -0,0 +1,5 @@
+def program_1():
+    print('KONIEC !')
+
+if __name__ == '__main__':
+    print('Początek funkcji')
